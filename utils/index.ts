d5300c121756958cfffced942e7b7e6d0a860a55
aakindel <a.akindele01@gmail.com>
2023-11-14 04:48:03 -0700

    update utils

--- utils/index.ts
+++ utils/index.ts
@@ -5,3 +5,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// from https://stackrant.com/posts/tiltable-cards
+export function round(num: number, fix = 2) {
+  return parseFloat(num.toFixed(fix));
+}
+
+// from https://stackrant.com/posts/tiltable-cards
+export function distance(x1: number, y1: number, x2: number, y2: number) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
