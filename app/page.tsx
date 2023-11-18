601ea0a7684d7e844ec93561ebd9c05a99a32826
aakindel <a.akindele01@gmail.com>
2023-11-18 13:21:42 -0700

    fetch stripe data from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
+"use client";
+
 import { MainNav } from "@/components/main-nav";
 import { PricingCard } from "@/components/pricing-card";
+import { useState, useEffect } from "react";
 import type { NextPage } from "next";
+import Stripe from "stripe";
+import axios from "axios";
 
 const Home: NextPage = () => {
+  const [stripeProducts, setStripeProducts] = useState<Stripe.Price[]>([]);
+
+  useEffect(() => {
+    fetchStripeProducts();
+  }, []);
+
+  const fetchStripeProducts = async () => {
+    const { data } = await axios.get("/api/get-products");
+    setStripeProducts(data);
+    console.log(data);
+  };
+
   return (
     <div className="mx-auto min-h-screen w-full">
       <MainNav />
       <div className="mx-auto flex h-[calc(100vh-57px)] w-full max-w-[1400px] items-center justify-center px-4 sm:px-8">
-        <PricingCard />
+        {stripeProducts &&
+          stripeProducts.map((stripeProduct) => (
+            <PricingCard key={stripeProduct.id} stripeProduct={stripeProduct} />
+          ))}
       </div>
     </div>
   );
